6d1c5d856e609f54aa2c016e89d5a7ded5305381
Mirco Dotta <mirco.dotta@gmail.com>
2017-12-27 17:38:17 +0100

    Added user ObjectType derivation

--- src/main/scala/bug/Derivation.scala
+++ src/main/scala/bug/Derivation.scala
@@ -3,5 +3,8 @@ package bug
 import sangria.macros.derive._
 
 object Derivation {
-  val MutationType = deriveContextObjectType[MyCtx, Mutation, Unit](_.mutation)
+
+  val MutationType = deriveContextObjectType[MyCtx, Mutation, Unit](_.mutation
+    /*,  IncludeMethods("addUser")*/ // Uncomment this and compilation will succeed
+  )
 }
--- src/main/scala/bug/User.scala
+++ src/main/scala/bug/User.scala
@@ -1,3 +1,7 @@
 package bug
 
 case class User(firstName: String, lastName: Option[String])
+object User {
+  import sangria.macros.derive.deriveObjectType
+  implicit val UserType = deriveObjectType[MyCtx, User]()
+}
